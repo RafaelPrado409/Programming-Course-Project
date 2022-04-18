495c6e3951e5d826d45eb1c4d8b150d34ea9a892
Rafael Prado <43122545+RafaelPrado409@users.noreply.github.com>
2022-04-18 19:13:56 -0300

    Add files via upload

---
+++ Backend/classroom/src/http/graphql/resolvers/student.resolver.ts
@@ -0,0 +1,33 @@
+import { UseGuards } from '@nestjs/common';
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+
+import { EnrollmentsService } from '../../../services/enrollments.service';
+import { StudentsService } from '../../../services/student.service';
+import { AuthorizationGuard } from '../../auth/authorization.guard';
+
+import { Student } from '../models/student';
+
+@Resolver(() => Student)
+export class StudentsResolver {
+  constructor(
+    private studentsService: StudentsService,
+    private enrollmentsService: EnrollmentsService,
+  ) {}
+
+  // @UseGuards(AuthorizationGuard)
+  // @Query(() => Student)
+  // me(@CurrentUser() user: AuthUser) {
+  //   return this.studentsService.getStudentByAuthUserId(user.sub);
+  // }
+
+  @Query(() => [Student])
+  @UseGuards(AuthorizationGuard)
+  students() {
+    return this.studentsService.listAllStudents();
+  }
+
+  @ResolveField()
+  enrollments(@Parent() student: Student) {
+    return this.enrollmentsService.listEnrollmentsByStudent(student.id);
+  }
+}
